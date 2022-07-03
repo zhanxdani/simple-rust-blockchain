e9d50e23505402d357db46dbce4d493ad3153328
Zhantai Dan <zhantaidaniiarov@gmail.com>
2022-07-03 16:34:51 +0600

    New Block (big update)

--- src/block.rs
+++ src/block.rs
@@ -1,61 +1,77 @@
-//! A chain block.
+use std::fmt::{ self, Debug, Formatter };
+use super::*;
 
-use bincode::serialize;
-use sha1::Sha1;
-
-use hash_content::HashContent;
-
-#[derive(Serialize, Deserialize, Clone)]
 pub struct Block {
-    content: HashContent,
-    previous: String,
-    current: String,
+    pub index: u32,
+    pub timestamp: u128,
+    pub hash: Hash,
+    pub prev_block_hash: Hash,
+    pub nonce: u64,
+    pub transactions: Vec<Transaction>,
+    pub difficulty: u128,
+}
+
+impl Debug for Block {
+    fn fmt (&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "Block[{}]: {} at: {} with: {} nonce: {}",
+            &self.index,
+            &hex::encode(&self.hash),
+            &self.timestamp,
+            &self.transactions.len(),
+            &self.nonce,
+        )
+    }
 }
 
 impl Block {
-
-    /// One block constructor. Creates the block from the given data and previous digest. Calculates its own hash digest.
-    ///
-    /// Args:
-    ///
-    /// `data` - the data of the block
-    /// `previous` - the digest of the previous block (empty if genesis)
-    ///
-    /// Returns:
-    ///
-    /// new block
-    pub fn new(
-        data: i32,
-        previous: String,
-    ) -> Block {
-
-        let content = HashContent::new(data);
-        let bytes = serialize(&content).unwrap();
-        let digest = Sha1::from(bytes).hexdigest();
-
+    pub fn new (index: u32, timestamp: u128, prev_block_hash: Hash, transactions: Vec<Transaction>, difficulty: u128) -> Self {
         Block {
-            content: content,
-            previous: previous,
-            current: digest,
+            index,
+            timestamp,
+            hash: vec![0; 32],
+            prev_block_hash,
+            nonce: 0,
+            transactions,
+            difficulty,
         }
     }
 
-    /// Getter of the current block hash digest.
-    ///
-    /// Returns:
-    ///
-    /// current block digest as string
-    pub fn get_current(&self) -> &str {
-        &self.current
-    }
-
-    /// Getter of the hashed content.
-    ///
-    /// Returns:
-    ///
-    /// block hashed content
-    pub fn get_content(&self) -> &HashContent {
-        &self.content
+    pub fn mine (&mut self) {
+        for nonce_attempt in 0..(u64::max_value()) {
+            self.nonce = nonce_attempt;
+            let hash = self.hash();
+            if check_difficulty(&hash, self.difficulty) {
+                self.hash = hash;
+                return;
+            }
+        }
     }
 }
+
+impl Hashable for Block {
+    fn bytes (&self) -> Vec<u8> {
+        let mut bytes = vec![];
+
+        bytes.extend(&u32_bytes(&self.index));
+        bytes.extend(&u128_bytes(&self.timestamp));
+        bytes.extend(&self.prev_block_hash);
+        bytes.extend(&u64_bytes(&self.nonce));
+        bytes.extend(
+            self.transactions
+                .iter()
+                .flat_map(|transaction| transaction.bytes())
+                .collect::<Vec<u8>>()
+        );
+        bytes.extend(&u128_bytes(&self.difficulty));
+
+        bytes
+    }
+}
+
+pub fn check_difficulty (hash: &Hash, difficulty: u128) -> bool {
+    difficulty > difficulty_bytes_as_u128(&hash)
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
 
